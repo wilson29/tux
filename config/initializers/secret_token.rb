efd3e92c57280f4b6f53f1737c0aef21a7ca7c6c
Scott Wilson <1upgreenshroom@gmail.com>
2012-01-22 10:39:04 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tux::Application.config.secret_token = 'f9ae4d0ea382536f43457461f02391e233ac6df61efe4bb4e01e29830612be2e47b15fe7b1b51f724e983b0b489d54a9d4c9e441ea710e96593c73ac1f901a1a'
